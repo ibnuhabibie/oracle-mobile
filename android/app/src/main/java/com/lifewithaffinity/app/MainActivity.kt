b363e33f130773c2562d2f241bce907b09e4ad5d
Ibnu Habibe <m.habibieibnu@gmail.com>
2026-03-23 01:30:32 +0700

    change package name android

--- android/app/src/main/java/com/lifewithaffinity/app/MainActivity.kt
+++ android/app/src/main/java/com/lifewithaffinity/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.theaffinity.app
+package com.lifewithaffinity.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/lifewithaffinity/app/MainApplication.kt
+++ android/app/src/main/java/com/lifewithaffinity/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.theaffinity.app
+package com.lifewithaffinity.app
 
 import android.app.Application
 import com.facebook.react.PackageList
